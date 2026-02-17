ae0b91ec25ee0ab832a40701edfd2f49cbbe1de2
Phan <kaitokid832001@gmail.com>
2026-02-17 11:53:02 +0300

    data_ v5

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -11,33 +11,18 @@ public class Calculator {
     }
 
 
-    public double calculateCircleArea(double radius) {
-        return 3.14 * radius * radius;
-    }
-
     public int subtract(int a, int b) {
         return a - b;
     }
 
     public int divide(int a, int b) {
-        return a / b; // Уязвимость: деление на ноль!
-    }
-
-    public String fizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(number);
-        }
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero");
+        return a / b;
     }
 
     public boolean isPrime(int n) {
         if (n <= 1) return false;
-        for (int i = 2; i < n; i++) { // Должен быть i <= Math.sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
